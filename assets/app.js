c53f7319c4e7bf1d4900268135f5cab91e02a031
Sabine <sabine.moreira95@gmail.com>
2021-05-20 00:05:51 +0200

    made success payement template design

--- assets/app.js
+++ assets/app.js
@@ -18,6 +18,9 @@ const $ = require('jquery');
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
 
+
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 // or you can include specific pieces
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
