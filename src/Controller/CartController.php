caafd38153b61bb17540424945d9496c81bdd684
Sabine <sabine.moreira95@gmail.com>
2021-05-19 13:11:28 +0200

    changed desing panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
-use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
